1685a0998f6c0d9caeecdc6c6ae61907b12c6478
David Braden <davidbraden@gmail.com>
2016-08-02 20:56:26 +0100

    Initial commit

---
+++ src/main/java/com/davidbraden/address/book/api/WebApplication.java
@@ -0,0 +1,29 @@
+package com.davidbraden.address.book.api;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class WebApplication extends Application<WebConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new WebApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "address-book-api";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<WebConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final WebConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/com/davidbraden/address/book/api/WebConfiguration.java
@@ -0,0 +1,10 @@
+package com.davidbraden.address.book.api;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class WebConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
